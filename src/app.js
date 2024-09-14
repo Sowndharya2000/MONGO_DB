a01d780f0585499ca2cfffe5d8cde57a1eaacb95
Venkatesan89 <v.sowndharya2000@gmail.com>
2024-09-14 18:24:04 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express();
+let port = 5000;
+const path = require('path');
+const template_path = path.join(__dirname, "../template/views");
+const empcollection = require("./model/model");
+app.set('view engine','hbs');
+app.set('views',template_path);
+const mongoose = require('mongoose')
+
+app.use(express.urlencoded({extended:true}));
+require('./db/db');
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+
+
+    
+    app.listen(port,()=>{
+        console.log("listening to the port ${port}");
+    })
+
+    app.post('/empdata', async(req,res)=>{
+      
+            const{regd_no,name,email,branch} = req.body
+            const user = new Users({
+            regd_no,
+            name,
+            email,
+            branch
+            })
+            await user.save()
+            console.log(user)  
+        res.send("<b>Form Submitted Successfully</b>")
+        })
+
+        
+     
+        const userSchema = new mongoose.Schema({
+            regd_no:String,
+            name:String,
+            email:String,
+            branch:String
+          })
+          const Users = mongoose.model('empdata',userSchema)  
+       
+     
+
+
+
